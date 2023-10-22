fa9457dc7822a80a943df2d13e21cf7d0e6950d8
ZhangMin1998 <1464253423@qq.com>
2023-10-22 23:44:01 +0800

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -12,11 +12,11 @@ import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'HomeView',
   components: {
-    // HelloWorld
+    HelloWorld
   },
   methods: {
     aaa () {
-      const a = 111
+      const aa = 111
       // console.log(a)
     }
   }
