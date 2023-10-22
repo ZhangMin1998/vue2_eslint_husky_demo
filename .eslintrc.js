2703296d75133c0e21f156cb2b015e93d5b6b3e5
ZhangMin1998 <1464253423@qq.com>
2023-10-22 23:35:17 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,13 @@ module.exports = {
     parser: '@babel/eslint-parser'
   },
   rules: {
+    /**
+     * "off" 或 0 - 关闭规则
+     * "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出),
+     * "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
+     */
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 2
   }
 }
